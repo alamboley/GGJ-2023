d242ae166b30d3a3fbeaf618c12a5406b8c4ded6
Aymeric <aymeric.lamboley@gmail.com>
2023-02-04 00:40:11 +0100

    can move in all direction

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -16,38 +16,75 @@ public class Character : MonoBehaviour
 
     void Update()
     {
-        Debug.DrawRay(transform.position, Vector2.right, Color.cyan);
+        
         timeSinceLastMovement += Time.deltaTime;
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, 0.4f * 2);
-            if (hit.collider != null)
-            {
-
-                hit.collider.GetComponent<Blocks>().AddDamage(damage);
-
-                Debug.Log(hit.collider.gameObject.name);
-
-            }
-            else
-            {
-                if (timeSinceLastMovement >= speedMove)
-                {
-                    this.transform.Translate(0.4f, 0, 0);
-                    timeSinceLastMovement = 0f;
-                }
-                
-            }
+            MoveToDirection(0.4f, 0);
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
+            MoveToDirection(-0.4f, 0);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            MoveToDirection(0, 0.4f);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            MoveToDirection(0, -0.4f);
+        }
+    }
+
+    void MoveToDirection(float x, float y)
+    {
+        Vector2 direction = Vector2.zero;
+        Vector3 raycastCenter = transform.position;
+
+        if (x > 0)
+        {
+            direction = Vector2.right;
+            raycastCenter.y -= 0.4f / 2;
+        }
+        if (x < 0)
+        {
+            direction = Vector2.left;
+            raycastCenter.y -= 0.4f / 2;
+        }
+        if (y > 0)
+        {
+            direction = Vector2.up;
+            raycastCenter.x += 0.4f / 2;
+        }
+        if (y < 0)
+        {
+            direction = Vector2.down;
+            raycastCenter.x += 0.4f / 2;
+        }
+
+        Debug.DrawRay(raycastCenter, direction, Color.cyan);
+
+        RaycastHit2D hit = Physics2D.Raycast(raycastCenter, direction, 0.4f);
+
+        if (hit.collider != null)
+        {
+            Debug.Log(hit.collider.gameObject.name);
+            DamageBlock(hit.collider);
+        }
+        else
+        {
             if (timeSinceLastMovement >= speedMove)
             {
-                this.transform.Translate(-0.4f, 0, 0);
+                this.transform.Translate(x, y, 0);
                 timeSinceLastMovement = 0f;
             }
+
         }
+    }
+
+    void DamageBlock(Collider2D collider)
+    {
+        collider.GetComponent<Blocks>().AddDamage(damage);
     }
 }
