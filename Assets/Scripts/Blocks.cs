ce37b31d1fb3541c702b1443d10acf9ed4b2f330
Aymeric <aymeric.lamboley@gmail.com>
2023-02-04 16:23:45 +0100

    dynamite OK

--- Assets/Scripts/Blocks.cs
+++ Assets/Scripts/Blocks.cs
@@ -10,40 +10,38 @@ public class Blocks : MonoBehaviour
 
     [SerializeField] float health = 100f;
 
-    private void Update()
-    {
-
-    }
-
     public void AddDamage(float damage)
     {
         health -= damage;
 
         if (health < 0)
+            DestroyBlock();
+    }
+
+    public void DestroyBlock()
+    {
+        Vector3 raycastCenter = transform.position;
+        raycastCenter.x += 0.4f / 2;
+        raycastCenter.y += 0.4f / 2;
+
+        Debug.DrawRay(raycastCenter, Vector2.up, Color.cyan);
+
+        RaycastHit2D hit = Physics2D.Raycast(raycastCenter, Vector2.up, 0.4f / 2);
+
+        if (hit.collider != null)
         {
-            Vector3 raycastCenter = transform.position;
-            raycastCenter.x += 0.4f / 2;
-            raycastCenter.y += 0.4f / 2;
+            //Debug.Log("me :" + gameObject.name);
+            //Debug.Log(hit.collider.gameObject.name);
+            Gravity gravityBlock = hit.collider.GetComponent<Gravity>();
 
-            Debug.DrawRay(raycastCenter, Vector2.up, Color.cyan);
-
-            RaycastHit2D hit = Physics2D.Raycast(raycastCenter, Vector2.up, 0.4f / 2);
-
-            if (hit.collider != null)
+            if (gravityBlock != null)
             {
-                //Debug.Log("me :" + gameObject.name);
-                //Debug.Log(hit.collider.gameObject.name);
-                Gravity gravityBlock = hit.collider.GetComponent<Gravity>();
-
-                if (gravityBlock != null)
-                {
-                    gravityBlock.BottomBlockDestroying();
-                }
+                gravityBlock.BottomBlockDestroying();
             }
-
-            GameManager.Instance.AddMoneyBlockDestroyed(coin);
-
-            Destroy(gameObject);
         }
+
+        GameManager.Instance.AddMoneyBlockDestroyed(coin);
+
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/Dynamite.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+
+public class Dynamite : MonoBehaviour
+{
+    void Start()
+    {
+        Vector2 direction = Vector2.zero;
+        Vector3 raycastCenter = transform.position;
+
+        direction = Vector2.right;
+        Vector3 raycastRight = transform.position;
+        raycastRight.x += 0.4f / 2 + 0.4f;
+        raycastRight.y -= 0.4f / 2;
+        Debug.DrawRay(raycastRight, direction, Color.cyan);
+        RaycastResult(Physics2D.Raycast(raycastRight, direction, 0.4f / 2));
+
+        direction = new Vector2(1, 1);
+        Vector3 raycastUpRight = transform.position;
+        raycastUpRight.x += 0.4f / 2 + 0.4f;
+        raycastUpRight.y += 0.4f / 2;
+        Debug.DrawRay(raycastUpRight, direction, Color.cyan);
+        RaycastResult(Physics2D.Raycast(raycastUpRight, direction, 0.4f / 2));
+
+        direction = Vector2.left;
+        Vector3 raycastLeft = transform.position;
+        raycastLeft.x += 0.4f / 2 - 0.4f;
+        raycastLeft.y -= 0.4f / 2;
+        Debug.DrawRay(raycastLeft, direction, Color.cyan);
+        RaycastResult(Physics2D.Raycast(raycastLeft, direction, 0.4f / 2));
+
+        direction = new Vector2(-1, 1);
+        Vector3 raycastUpLeft = transform.position;
+        raycastUpLeft.x += 0.4f / 2 - 0.4f;
+        raycastUpLeft.y += 0.4f / 2;
+        Debug.DrawRay(raycastUpLeft, direction, Color.cyan);
+        RaycastResult(Physics2D.Raycast(raycastUpLeft, direction, 0.4f / 2));
+
+        direction = Vector2.up;
+        Vector3 raycastUp = transform.position;
+        raycastUp.x += 0.4f / 2;
+        raycastUp.y -= 0.4f / 2 - 0.4f;
+        Debug.DrawRay(raycastUp, direction, Color.cyan);
+        RaycastResult(Physics2D.Raycast(raycastUp, direction, 0.4f / 2));
+
+        direction = new Vector2(1, -1);
+        Vector3 raycastDownRight = transform.position;
+        raycastDownRight.x += 0.4f / 2 + 0.4f;
+        raycastDownRight.y -= 0.4f / 2 + 0.4f;
+        Debug.DrawRay(raycastDownRight, direction, Color.cyan);
+        RaycastResult(Physics2D.Raycast(raycastDownRight, direction, 0.4f / 2));
+
+        direction = Vector2.down;
+        Vector3 raycastDown = transform.position;
+        raycastDown.x += 0.4f / 2;
+        raycastDown.y -= 0.4f / 2 + 0.4f;
+        Debug.DrawRay(raycastDown, direction, Color.cyan);
+        RaycastResult(Physics2D.Raycast(raycastDown, direction, 0.4f / 2));
+
+        direction = new Vector2(-1, -1);
+        Vector3 raycastDownLeft = transform.position;
+        raycastDownLeft.x += 0.4f / 2 - 0.4f;
+        raycastDownLeft.y -= 0.4f / 2 + 0.4f;
+        Debug.DrawRay(raycastDownLeft, direction, Color.cyan);
+        RaycastResult(Physics2D.Raycast(raycastDownLeft, direction, 0.4f / 2));
+    }
+
+    void RaycastResult(RaycastHit2D hit)
+    {
+        if (hit.collider != null)
+        {
+            Blocks block = hit.collider.GetComponent<Blocks>();
+            if (block != null)
+                block.DestroyBlock();
+
+            Character character = hit.collider.GetComponent<Character>();
+            if (character != null)
+            {
+                Debug.Log("DYNAMITE KILLS PLAYERS");
+                Destroy(hit.collider.gameObject);
+            }
+        }
+    }
+}
